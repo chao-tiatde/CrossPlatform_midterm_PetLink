d542df18553793b3e67c48fae04f096599204442
mr77tobey <s111219005@stu.ntue.edu.tw>
2025-04-16 17:40:06 +0800

    唉

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-// import 'antd/dist/reset.css'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import './App.css'
-import Home from "./pages/Home"
-import Product from "./pages/Product"
-import Species from './pages/Species';
-import ScrollToTop from './components/ScrollToTop';
-import Intro from './pages/Intro/Intro';
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./App.css";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Species from "./pages/Species";
+import ScrollToTop from "./components/ScrollToTop";
+import Intro from "./pages/Intro/Intro";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="introduction" element={<Intro />}/>
-          <Route path=":productSpecies" element={<Species />}>
-            <Route path="id/:productId" element={<Product />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="introduction" element={<Intro />} />
+        <Route path=":productSpecies" element={<Species />} />
+        <Route path=":productSpecies/:productId" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
